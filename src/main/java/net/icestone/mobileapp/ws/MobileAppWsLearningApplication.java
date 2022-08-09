8e63d9f9cac4bd2dbfcb60e7024a1fd297d6c730
Douglas Fung <icestone.net@gmail.com>
2022-08-09 21:18:36 +0800

    initial commit

---
+++ src/main/java/net/icestone/mobileapp/ws/MobileAppWsLearningApplication.java
@@ -0,0 +1,13 @@
+package net.icestone.mobileapp.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWsLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWsLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/icestone/mobileapp/ws/MobileAppWsLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package net.icestone.mobileapp.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
