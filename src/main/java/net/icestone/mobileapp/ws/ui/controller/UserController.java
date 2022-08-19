0c5846842582840b703c59c0494bb84b505b514a
Douglas Fung <icestone.net@gmail.com>
2022-08-19 08:34:04 +0800

    Up to 133 Adding Links. Representation Model

--- src/main/java/net/icestone/mobileapp/ws/ui/controller/UserController.java
+++ src/main/java/net/icestone/mobileapp/ws/ui/controller/UserController.java
@@ -8,6 +8,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -197,12 +199,30 @@ public class UserController {
 		AddressDTO addressesDto = addressService.getAddress(addressId);
 
 		ModelMapper modelMapper = new ModelMapper();
+		
+		AddressesRest returnValue = modelMapper.map(addressesDto, AddressesRest.class);
+		
+		Link userLink = WebMvcLinkBuilder.linkTo(UserController.class).slash(userId).withRel("user");
+		
+		Link userAddressesLink = WebMvcLinkBuilder.linkTo(UserController.class)
+				.slash(userId)
+				.slash("addresses")
+				.withRel("addresses");
+		
+		Link selfLink = WebMvcLinkBuilder.linkTo(UserController.class)
+				.slash(userId)
+				.slash("addresses")
+				.slash(addressId)
+				.withSelfRel();
+		
+		
+		returnValue.add(userLink);
+		returnValue.add(userAddressesLink);
+		returnValue.add(selfLink);
 
-		AddressesRest addressesRestModel = modelMapper.map(addressesDto, AddressesRest.class);
-
-
-		return addressesRestModel;
+		return returnValue;
 	}
+
 
 	
 }
